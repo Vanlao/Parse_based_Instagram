30bfff5f48e94392cf60e2a85bd6d2100039ee9a
Tu Pham <tupham@tus-macbook-pro.local>
2018-09-30 20:41:38 -0700

    Initial Commit

---
+++ Parse_based_InstagramTests/Parse_based_InstagramTests.swift
@@ -0,0 +1,34 @@
+//
+//  Parse_based_InstagramTests.swift
+//  Parse_based_InstagramTests
+//
+//  Created by Tu Pham on 9/30/18.
+//  Copyright © 2018 Van Lao. All rights reserved.
+//
+
+import XCTest
+@testable import Parse_based_Instagram
+
+class Parse_based_InstagramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
